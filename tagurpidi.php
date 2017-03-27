57deda03897ffaeb46dcae86a359e3f821c95007
Priit Rätsep <pratsep@itcollege.ee>
2017-03-27 12:32:32 +0300

    Funktsioon tagurpidi tekstile

--- tagurpidi.php
+++ tagurpidi.php
@@ -8,10 +8,15 @@
 <body>
     <?php
         $tekst = "See tekst esitatakse tagurpidi!";
-        $massiiv = str_split($tekst);
-        for($i = count($massiiv)-1; $i > -1; $i--){
-            echo $massiiv[$i];
+        echo $tekst;
+        echo "</br>";
+        function tagurpidi($sisestus){
+            $massiiv = str_split($sisestus);
+            for($i = count($massiiv)-1; $i > -1; $i--){
+                echo $massiiv[$i];
+            }
         }
+        tagurpidi($tekst);
     ?>
 </body>
 </html>
